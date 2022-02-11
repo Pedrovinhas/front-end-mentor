ba4ea80587a5bd6ccb76c9b2c57ec3cfe29c4cf7
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2022-02-10 23:01:18 -0300

    add popup menu

---
+++ newbie/article-preview-component/js/script.js
@@ -0,0 +1,8 @@
+const popUp = document.querySelector('#share-img');
+const modal = document.querySelector('.desktop-share');
+
+popUp.addEventListener("click", () => {
+    console.log("Click")
+    modal.classList.add("active");
+})
+
