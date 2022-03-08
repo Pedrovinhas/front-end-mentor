90be54e58bb4b5aa610ae02683bee60e6fdf575e
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2022-03-08 00:01:38 -0300

    add javascript menu

--- junior/blogr-landing-page-main/js/menu.js
+++ junior/blogr-landing-page-main/js/menu.js
@@ -1,7 +1,21 @@
 const arrows = document.querySelectorAll('#arrow');
+const popupMenu = document.querySelectorAll('.popup-list');
 
 arrows.forEach(arrow => {
-    arrow.addEventListener('mouseover', () => {
-        console.log('oi')
+    arrow.addEventListener('click', () => {
+
+        if(arrows[1]) {
+            arrows[1].style.transform = 'rotate(180deg)';
+            popupMenu[1].style.transform = '1000';
+            popupMenu[1].style.display = 'flex';
+            
+        }
+
+  
+       
     })
 })
+
+const menuPopUp = () => {
+
+}
